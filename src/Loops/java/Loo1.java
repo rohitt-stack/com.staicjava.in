b624466c8792c66e2afe63fd4901da0695cd13fb
rohitt-stack <rk645147529@gmil.com>
2026-06-13 15:42:58 +0530

    Added Arrays and Loops programs

---
+++ src/Loops/java/Loo1.java
@@ -0,0 +1,21 @@
+package Loops.java;
+
+import  java.util.Scanner;
+
+public class Loo1 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            sum = sum + i;
+        }
+
+        System.out.println("Sum = " + sum);
+    }
+}
