ea1cc40367d226401522b9d304be59cc8b7c98be
rohitt-stack <rk645147529@gmil.com>
2026-06-13 15:16:51 +0530

    Added Arrays,Condition,Loops and Methods program

---
+++ src/Methods/java/Met1.java
@@ -0,0 +1,19 @@
+package Methods.java;
+
+import java.util.Scanner;
+
+public class Met1 {
+    static void pattern(int n){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i+1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        pattern(n);
+    }
+}
