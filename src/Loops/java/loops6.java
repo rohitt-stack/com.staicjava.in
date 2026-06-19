5753918522e9ddf882a0aa7750fb6292d1214158
rohitt-stack <rk645147529@gmil.com>
2026-06-19 22:06:05 +0530

    Added Loops and method programs

---
+++ src/Loops/java/loops6.java
@@ -0,0 +1,31 @@
+package Loops.java;
+
+public class loops6 {
+   /* public static void main(String[] args) {
+        for (int i=1; i<=100; i++){
+            if(i%3==0){
+                System.out.println(i);
+            }
+        }
+    }
+}*/
+
+    // 1 se 20 tak number print karo. lekin 5 ko skip karo!
+   /* public static void main(String[] args) {
+        for (int i = 1; i <= 20; i++) {
+            if(i == 5) {
+                continue;
+            }
+                System.out.println(i );
+            }
+        }
+    }*/
+
+    static void main(String[] args) {
+        for (int i=10;i>=1;i--) {
+            System.out.println(i);
+        }
+    }
+    }
+
+
---
+++ src/Methods/java/Met01.java
@@ -0,0 +1,25 @@
+package Methods.java;
+
+public class Met01.java{
+    static int subtract(int a, int b){
+    return a - b;
+    }
+}
+public static void main(String[] args) {
+    int a = 12;
+    int b = 5;
+    System.out.println(subtract(a, b));
+}
+}
+
+public class Met01 {
+    static int grtest(int a, int b){
+        return a - b;
+    }
+}
+public static void main(String[] args) {
+    int a = 12;
+    int b = 5;
+    System.out.println(subtract(a, b));
+}
+}
